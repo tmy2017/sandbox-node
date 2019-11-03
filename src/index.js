9cb57530b66b06f18fa764d7219b0505fc68a497
Tmy2017 <30006459+tmy2017@users.noreply.github.com>
2019-11-03 19:12:15 +0100

    commit works if start from template

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 server.listen(3000, () => {
-  console.log("JSON Server is running");
+  console.log("JSON Server is running cool");
 });
 
 // var http = require("http");
